4af58b00fcc7ee9c5bd068eaa54d3439ece13ce2
git <github@example.com>
2025-10-13 13:00:12 +0000

    Fixing Codacy Issues

--- dbt_train/tests/bronze/fact_sales_test.jinja.sql
+++ dbt_train/tests/bronze/fact_sales_test.jinja.sql
@@ -1,6 +1,6 @@
 select
     sales_id,
     sum(net_amount) as total_amount
-from {{ source("source", "fact_sales") }}
+from {{ source("source", "RAC_fact_sales") }}
 group by 1
 having total_amount < 0
